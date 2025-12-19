199035607f75f57da2a02c2b65ebd1f5b8ea5e99
Daniil6537 <3880633@gmail.com>
2025-12-19 12:21:24 +0000

    laba8

---
+++ laba8.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int M, N;
+    scanf("%d %d", &M, &N);
+
+    int a[M][N];
+
+    // Ввод матрицы
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Находим максимальный элемент в каждом столбце
+    int minOfMax;
+
+    for (int j = 0; j < N; j++) {
+        int maxInCol = a[0][j];
+        for (int i = 1; i < M; i++) {
+            if (a[i][j] > maxInCol) {
+                maxInCol = a[i][j];
+            }
+        }
+
+        if (j == 0 || maxInCol < minOfMax) {
+            minOfMax = maxInCol;
+        }
+    }
+
+    printf("%d\n", minOfMax);
+
+    return 0;
+}
