3cc7ea2553f484bc2f232864fab33201c83dfcfe
Daniil6537 <3880633@gmail.com>
2025-12-19 12:24:29 +0000

    laba9

---
+++ laba9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Процедура добавления цифры D справа к числу K
+void AddRightDigit(int D, int *K) {
+    *K = (*K) * 10 + D;
+}
+
+int main() {
+    int K, D1, D2;
+
+    scanf("%d %d %d", &K, &D1, &D2);
+
+    AddRightDigit(D1, &K);
+    printf("%d\n", K);
+
+    AddRightDigit(D2, &K);
+    printf("%d\n", K);
+
+    return 0;
+}
