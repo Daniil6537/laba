67378df6aee1efd6de513d93c83b90afa0f156d4
Daniil6537 <3880633@gmail.com>
2025-12-19 09:41:27 +0000

    лаба1

--- laba01.c
+++ laba01.c
@@ -1,1 +1,14 @@
-Лабараторная 1 сделана
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a = n / 10;   // десятки
+    int b = n % 10;   // единицы
+
+    int result = b * 10 + a;
+    printf("%d\n", result);
+
+    return 0;
+}
